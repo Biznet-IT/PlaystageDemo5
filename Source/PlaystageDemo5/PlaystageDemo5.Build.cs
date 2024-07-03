385c9a05361f38c8358d26384b3e0fe55b359f5a
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-07-03 12:36:39 -0400

    Importacion Assets desde PlaystageDemo4 12

--- Source/PlaystageDemo5/PlaystageDemo5.Build.cs
+++ Source/PlaystageDemo5/PlaystageDemo5.Build.cs
@@ -8,7 +8,7 @@ public class PlaystageDemo5 : ModuleRules
 	{
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "Json" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "Json", "Networking", "Sockets"  });
 
         PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -16,7 +16,7 @@ public class PlaystageDemo5 : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
